6d1b61379abcef0faa1bef5ed5d7f1f87a7fdd51
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-10-17 11:16:24 +0200

    min max trie

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 // // TAB 1
-let tab1 = [];
-for (i = 0; tab1.length < 5; i++) {
-  let valeurTab1 = Number(prompt("Veuillez saisir les valeurs du tableau 1"));
-  tab1.push(valeurTab1);
-}
-console.log(tab1);
+// let tab1 = [];
+// for (i = 0; tab1.length < 5; i++) {
+//   let valeurTab1 = Number(prompt("Veuillez saisir les valeurs du tableau 1"));
+//   tab1.push(valeurTab1);
+// }
+// console.log(tab1);
 // TAB 2
 let tab2 = [];
-for (i = 0; tab2.length < 5; i++) {
-  let valeurTab2 = Number(prompt("Veuillez saisir les valeurs du tableau 2"));
-  tab2.push(valeurTab2);
-}
-console.log(tab2);
+// for (i = 0; tab2.length < 5; i++) {
+//   let valeurTab2 = Number(prompt("Veuillez saisir les valeurs du tableau 2"));
+//   tab2.push(valeurTab2);
+// }
+// console.log(tab2);
 // TAB 3 A PARTIR DES ELEMENTS DE TAB1 absents dans TAB2
 let tab3 = [10, 20, 24, 78];
-console.log(tab3);
+// console.log(tab3);
 // TAB 4 A PARTIR DES ELEMENTS DU TAB1 presents dans TAB2
 let tab4 = [];
-console.log(tab4);
+// console.log(tab4);
 // // Ecrire une fonction avec comme paramètre tab1 et qui le trie
-function myFunction(tab1) {
-  tab1 = [10, 20, 24, 78, 1];
-  tab1.sort();
-  return tab1;
-}
-let tabTrier = myFunction(10, 20, 24, 78, 1);
-console.log(tabTrier);
+let Tab1 = [5, 3, 87, 1, -4, -99, 0];
+let tabTrie = Tab1.sort((a, b) => a - b);
+console.log(tabTrie);
+
 // Ecrire une fonction avec comme paramètre tab1 et qui retourne la valeur min max
+// Valeur maximale
+function findMax(tab) {
+  return Math.max.apply(null, tab);
+}
+console.log(findMax(Tab1));
+// Valeur minimale
+function findMin(tab) {
+  return Math.min.apply(null, tab);
+}
+console.log(findMin(Tab1));
